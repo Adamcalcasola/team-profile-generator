69f45067c28fada03f0d9697635954c673eb7f95
Adam Calcasola <adam.calcasola@gmail.com>
2021-12-04 14:54:02 -0500

    create array of objects using inquirer

--- index.js
+++ index.js
@@ -1,68 +1,256 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
-const Employee = require('./lib/Employee.js');
+//const {writeFile, copyFile} = require('./utils/generate-site.js');
+// const router = require('express').Router();
+// const path = require('path');
+// const fs = require('fs');
+//const Employee = require('../lib/Employee.js');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Employee = require('./lib/Employee');
 
-function createTeam() {
-    new Manager
-    employeeType();
-
+const createTeam = () => {
 }
+let team = [];
 
-class Manager extends Employee {
-    constructor(name, id, email) {
-        super(name, id, email);
-            inquirer.prompt([
-                {
-                    type: 'input',
-                    name: 'office',
-                    message: "What is the employee's office number?"
+
+const createManager = () => {
+    return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "Enter your manager's name.",
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log('Enter a name!');
+                        return false;
+                    }
                 }
-            ])
-    }
-}
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Enter your manager's ID number.",
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log('Enter an ID!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Enter your manager's email address.",
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log('Enter an email!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'office',
+                message: "What is your manager's office number?",
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log('Enter an office!');
+                        return false;
+                    }
+                }
+            }
+        ])
+        .then(data => {
+            team.push(new Manager(data.name, data.id, data.email, data.office));
+        })
+};
 
-class Engineer extends Employee {
-    getGithub () {
-        inquirer.prompt([
+
+
+function createEngineer() {
+    return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "Enter the engineer's name.",
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log('Enter a name!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Enter the engineer's ID number.",
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log('Enter an ID!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Enter the engineer's email address.",
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log('Enter an email!');
+                        return false;
+                    }
+                }
+            },
             {
                 type: 'input',
                 name: 'github',
-                message: "Enter the engineer's Github username."
+                message: "Enter the engineer's GitHub account.",
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log('Enter a Github account!');
+                        return false;
+                    }
+                }
             }
         ])
-    }
+        .then(data => {
+            team.push(new Engineer(data.name, data.id, data.email, data.github));
+            createEmployee();
+        })
 }
 
-class Intern extends Employee {
-    getSchool () {
-        inquirer.prompt([
+function createIntern() {
+    return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "Enter the intern's name.",
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log('Enter a name!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Enter the intern's ID number.",
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log('Enter an ID!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Enter the intern's email address.",
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log('Enter an email!');
+                        return false;
+                    }
+                }
+            },
             {
                 type: 'input',
                 name: 'school',
-                message: "Enter the intern's school."
+                message: "Enter the intern's school.",
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log('Enter a school!');
+                        return false;
+                    }
+                }
             }
         ])
-    }
+        .then(data => {
+            team.push(new Intern(data.name, data.id, data.email, data.school));
+            createEmployee();
+        })
 }
 
-const employeeType = () => {
+function createEmployee() {
     return inquirer.prompt([
         {
-            type: 'list',
-            name: 'employeeType',
-            message: "What type of team member would you like to add?",
-            choices: ['engineer', 'intern']
-        }
+            type: 'confirm',
+            name: 'addNew',
+            message: 'Would you like to add a new team member?',
+            default: false
+        },
     ])
-    .then(({employeeType}) => {
-        if (employeeType === engineer) {
-            Engineer.getInfo;
-            Engineer.getGithub;
+    .then(confirm => {
+        if (confirm.addNew) {
+            return inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'role',
+                    message: 'What role are you assigning?',
+                    choices: ['engineer', 'intern']
+                },
+            ])
+            .then(type => {
+                switch (type.role) {
+                    case 'engineer':
+                        createEngineer();
+                        break;
+                    case 'intern':
+                        createIntern();
+                        break;
+                    default:
+                        console.log('No selection');
+                        break;
+                } 
+            })
         } else {
-            Intern.getInfo;
-            Intern.getSchool;
+            console.log(team);
         }
     })
-}
+};
 
-createTeam();
+createManager()
+    .then(createEmployee)
+
+    // .then(createEmployee)
+    // .then(data => {
+    //      console.log(data);
+    // });
+        //return generatePage(data);
+    //})
+    // .then(pageHTML => {
+    //     return writeFile(pageHTML);
+    // })
+    // .then(writeFileResponse => {
+    //     return copyFile();
+    // })
+    // .then(err => {
+    //     console.log(err);
+    // })
